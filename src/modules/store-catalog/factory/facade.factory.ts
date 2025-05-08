6de575d6bf40cfeadc93e6f2e35f5aa052f12e88
Lucca Dias <lucca.gdias07@gmail.com>
2025-05-08 20:11:33 -0300

    feat: [store-catalog] store catalog facade

---
+++ src/modules/store-catalog/factory/facade.factory.ts
@@ -0,0 +1,18 @@
+import AddProductUseCase from "../../product-adm/usecase/add-product/add-product.usecase";
+import StoreCatalogFacade from "../facade/store-catalog.facade";
+import ProductRepository from "../repository/product.repository";
+import FindAllProductsUseCase from "../usecase/find-all-products/find-all-products.usecase";
+import FindProductUseCase from "../usecase/find-product/find-product.usecase";
+
+export default class StoreCatalogFacadeFactory {
+    static create() {
+        const productRepository = new ProductRepository();
+        const findProductUseCase = new FindProductUseCase(productRepository);
+        const findAllProductsUseCase = new FindAllProductsUseCase(productRepository);
+        const storeCatalogFacade = new StoreCatalogFacade({
+            findUseCase: findProductUseCase,
+            findAllUseCase: findAllProductsUseCase,
+        });
+        return storeCatalogFacade;
+    }
+}
